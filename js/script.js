a509387972c53152d923027fd8f71004af83744f
Lucas Olimpio <lucasolimpio267@gmail.com>
2023-01-10 20:15:23 -0300

    adicionando validacao de formulario

--- js/script.js
+++ js/script.js
@@ -18,28 +18,64 @@ const Main = {
     compoundInterest: function(e) {
       e.preventDefault()
       
+      const secondScreen = document.querySelector('.secondscreen')
+      const name = document.forms['form'].name.value
+
       let monthlyFee = document.forms['form'].monthlyFee.value
       let interest_rate = document.forms['form'].interestrate.value
       let yearsTime = document.forms['form'].yearsTime.value
 
-      monthlyFee = parseFloat(monthlyFee.replace('R$','').replace(' ', ''))
-      interest_rate = parseFloat(interest_rate.replace(',' , '.'))/100
-      yearsTime = yearsTime * 12
+      let RemonthlyFee = parseFloat(monthlyFee.replace('R$','').replace(' ', ''))
+      let Reinterest_rate = parseFloat(interest_rate.replace(',' , '.'))/100
+
+      const valid = Main.Events.formValidation()
+      if (valid == false){
+        alert("Algo deu errado, cheque as informações.")
+        return
+      }
     
       fetch('https://api.mathjs.org/v4/', {
         method: 'POST',
         headers: {
           'Content-Type':'application/json'
         },
-        body: `{"expr": "${monthlyFee} * (((1 + ${interest_rate}) ^ ${yearsTime} - 1) / ${interest_rate})"}`
+        body: `{"expr": "${RemonthlyFee} * (((1 + ${Reinterest_rate}) ^ ${yearsTime * 12} - 1) / ${Reinterest_rate})"}`
       })
-      .then(res => res.json().then(data => Main.Events.responsePage(data)))
-      .catch( ()=> {console.log('erro')})
+      .then(res => res.json().then(data => {
+        const result = parseFloat(data.result).toFixed(2)
+
+        secondScreen.innerHTML = `
+        <h2>Cálculo de juros compostos</h2>
+        <p>Olá ${name}, juntando R$ ${monthlyFee} todo mês, você terá R$ ${result} em ${yearsTime} anos.</p>
+        <button>simular novamente</button>
+        `
+        }
+      ))
+      .catch( ()=> {alert('Ocorreu um erro, tente novamente')})
     },
-  
-    responsePage: function(data) {
-      console.log(data)
+
+    formValidation: function() {
+
+      const name = document.forms['form'].name.value
+      const monthlyFee = document.forms['form'].monthlyFee.value
+      const interest_rate = document.forms['form'].interestrate.value
+      const yearsTime = document.forms['form'].yearsTime.value
+
+      if (name == "" || name == null) {
+        return false
+      }
+      if( monthlyFee == '' || monthlyFee == null) {
+        return false
+      }
+      if( interest_rate == '' || interest_rate == null) {
+        return false
+      }
+      if( yearsTime == '' || yearsTime == null) {
+        return false
+      }
+      return true
     }
+    
   }
 }
 
